abefc42daeb980697cee2eb36f92b1415b7a71d3
yuna <yuna744@gmail.com>
2023-12-27 12:12:43 +0900

    1227 useMutate로 클라이언트에서 서버 데이터 변경 요청 가능

--- src/Hooks/UseUserQuery.js
+++ src/Hooks/UseUserQuery.js
@@ -1,54 +1,46 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-/*
-  fresh: 서버데이터를 최신으로 인식하는 상태 (refetch 필요없는 상태)
-  stale: 서버데이터를 오래된 상태로 인식하는 상태 (refetch 필요한 상태)
-  inactive: 서버데이터가 더이상 해당 컴포넌트에서 활용되지 않는 상태
-  cacheTime: inactive상태에서 얼마까지 데이터를 유지시킬지에 대한 시간값 (default: 1000 * 60 * 5ms 5분)
-  statleTime: 처음 dataFetching후 얼마뒤에 fresh -> stale로 변경할지에 대한 시간값 (default: 0ms)
-  queryKey : 고유 인자값
-*/
-
-// 데이터 목록 호출 함수
+//특정 순번의 서버 데이터 가져오는 fetching func, custom hook (useQuery)
 const fetchUser = async ({ queryKey }) => {
-  console.log(queryKey);
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${queryKey[1]}`
+  );
   return await response.json();
 };
 
-// 데이터 목록 호출 커스텀 훅
-export const useUserQuery = () => {
-  return useQuery(["users"], fetchUser, {
+//2- 컴포넌트에서 해당 훅 호출시 고유쿼리를 등록하면서 num값을 전달해서 fetching함수 호출
+//반환된 데이터값이 옵션값에 따라 caching처리되면서 반환됨
+export const useUserQuery = (num) => {
+  return useQuery(["users", num], fetchUser, {
     refetchOnWindowFocus: false,
-    refetchOnMount: false,
+    refetchOnMount: true,
     cacheTime: 1000 * 20,
     staleTime: 1000 * 0,
   });
 };
 
-//
-const deleteUser = async (num) => {
+//특정 순번의 서버 데이터를 변경하는 fetching func, cutom hook (useMutation)
+//순서6- 해당 호출되면 num순번의 데이터 객체에서 name값을 같이 전달된 userName 서버데이터를 실제 변경처리한다음 반환
+export const updateUser = async ([userName, num]) => {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${num[1]}`,
+    `https://jsonplaceholder.typicode.com/users/${num}`,
     {
-      method: "DELETE",
+      method: "PATCH",
+      body: JSON.stringify({ name: userName }),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
     }
   );
-  return await response.json();
+  const result = await response.json();
+  return result;
 };
-
-// 인수로 순번을 받아서 해당 순번의 서버 데이터를 삭제하는 커스텀 훅(useMutation)
-export const useDeleteQuery = () => {
-  // 기존 생성한 queryClient 인스턴스를 호출
-  // 해당 queryClient 인스턴스에서 활용할 수 있는  prototype method인 setQueryData라는 서버 데이터 변경요청 값을 등록하는 함수
+export const useUpdateUser = () => {
   const queryClient = useQueryClient();
-  // useMutation(비동기 데이터 변경 함수, 옵션 설정 객체{onSuccess: mutate요청이 성공적으로 수행되면 연결될 핸들러 함수})
-  // useMutation훅이 deleteUser라는 내부 fetching함수를 호출해서 서버 데이터를 변경 요청
-  return useMutation(deleteUser, {
-    // 서버 데이터 변경이 성공하면 변경된 서버 데이터 값을 다시 고유의 쿼리키로 등록해서 react-query로 비동기 데이터 관리
+  //순서5- mutate메서드 호출시 아래구문이 자동적으로 호출되면서
+  //등록된 updateUser함수 호출
+  return useMutation(updateUser, {
+    //순서7- updateUser함수로 서버값 변경시 성공적으로 일어나면
+    //해당 값을 인수로 받아서 고유 쿼리키 생성하면서 캐싱처리된 값 쿼리 클라이언트로 전역 관리
     onSuccess: (args) => {
-      // args 데이터값 인자
-      // 변경되면 새로운 고유 쿼리키를 만들어서 users의,1(id), blabla~ (수정,삭제,업데이트)
       queryClient.setQueryData(["users", args.id], args);
     },
   });
--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,29 +1,32 @@
-import { useDeleteQuery, useUserQuery } from "./hooks/useUserQuery";
+import { useState } from "react";
+import { useUpdateUser, useUserQuery } from "./hooks/useUserQuery";
 
 export default function UserName() {
-  const { isSuccess, data } = useUserQuery();
-  // console.log(isSuccess);
-  console.log(data);
+  //1- 해당 컴포넌트 마운트시 react-query훅으로 2번째를 호출 및 자동 캐싱처리
+  const { isSuccess, data } = useUserQuery(2);
+  const updateUser = useUpdateUser();
+  const [UserName, setUserName] = useState("");
 
-  const deleteUser = useDeleteQuery(); //객체
-  console.log(data);
+  //4- 폼 전송이벤트 발생시 input에 있는 변경할 사용자 이름과 변경할 데이터 순번을 배열로 묶어서 update커스텀 훅으로 활성화시킨 useMutaion객체를 가져와서 mutate메서드에 인수로 전달
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateUser.mutate([UserName, 2]);
+  };
+
   return (
     <div className="UserName">
       <h1>User Name</h1>
-      <button
-        onClick={() => {
-          deleteUser.mutate(2);
-        }}
-      >
-        유저 데이터 삭제
-      </button>
-      <ul>
-        {isSuccess ? (
-          data.map((user) => <li key={user.id}>{user.name}</li>)
-        ) : (
-          <p>Loading...</p>
-        )}
-      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={UserName || ""}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </form>
+
+      {/* 3-useQuery훅에 의해서 캐싱반환된 데이터값 출력 */}
+      <p>{isSuccess && data.name}</p>
     </div>
   );
 }
